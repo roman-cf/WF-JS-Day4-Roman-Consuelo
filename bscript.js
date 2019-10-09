31087ac617222df13768db055541fcd2b104f1de
roman.cf <roman.springer@outlook.com>
2019-10-09 13:08:56 +0200

    intermediate fertig

--- bscript.js
+++ bscript.js
@@ -15,82 +15,114 @@ document.getElementById("output1").innerHTML = testobejt;
 
 // ---------------------------------------------------------------------------------------------
 
-var JSONob = {
-	"employees": [{
+var jsonOb = [
+		{
+		"id": "ID",
+		"firstname" : "Firstname",
+		"lastname" : "Lastname",
+		"emailadress" : "emailadress",
+		"jobtitle" : "Job Title",
+		"salary" : "Salary"
+		},
+		{
 		"id": 1,
 		"firstname" : "Anton",
 		"lastname" : "Müller",
 		"emailadress" : "email@internet.com",
 		"jobtitle" : "Developer",
-		"salary" : 3000.00,
+		"salary" : 3000.00
 		},
 		{
 		"id": 2,
-		"firstname" : "Anton",
-		"lastname" : "Müller",
+		"firstname" : "Bernhard",
+		"lastname" : "Mayer",
 		"emailadress" : "email2@internet.com",
 		"jobtitle" : "Developer",
-		"salary" : 3780.00,
+		"salary" : 3780.00
 		},
 		{
 		"id": 3,
-		"firstname" : "Anton",
-		"lastname" : "Müller",
+		"firstname" : "Chritian",
+		"lastname" : "Kunert",
 		"emailadress" : "email3@internet.com",
 		"jobtitle" : "Developer",
 		"salary" : 3000.00,
 		},		{
 		"id": 4,
-		"firstname" : "Anton",
-		"lastname" : "Müller",
+		"firstname" : "David",
+		"lastname" : "Frank",
 		"emailadress" : "email4@internet.com",
 		"jobtitle" : "Developer",
 		"salary" : 3600.00,
 		},		{
 		"id": 5,
-		"firstname" : "Anton",
-		"lastname" : "Müller",
+		"firstname" : "Edi",
+		"lastname" : "Langer",
 		"emailadress" : "emailfuenf@internet.com",
 		"jobtitle" : "Developer",
 		"salary" : 3000.00,
 		},		{
 		"id": 6,
-		"firstname" : "Anton",
-		"lastname" : "Müller",
+		"firstname" : "Fritz",
+		"lastname" : "Jäger",
 		"emailadress" : "emailsechs@internet.com",
 		"jobtitle" : "Developer",
 		"salary" : 1500.00,
 		},		{
 		"id":  7,
-		"firstname" : "Anton",
-		"lastname" : "Müller",
+		"firstname" : "Georg",
+		"lastname" : "Kaufmann",
 		"emailadress" : "emailsieben@internet.com",
 		"jobtitle" : "Developer",
 		"salary" : 2850.00,
 		},		{
 		"id": 8,
-		"firstname" : "Anton",
-		"lastname" : "Müller",
+		"firstname" : "Herbert",
+		"lastname" : "Weber",
 		"emailadress" : "emailacht@internet.com",
 		"jobtitle" : "Developer",
 		"salary" : 3500.00,
 		},		{
 		"id": 9,
-		"firstname" : "Anton",
-		"lastname" : "Müller",
+		"firstname" : "Irmgard",
+		"lastname" : "Landauer",
 		"emailadress" : "emailneun@internet.com",
 		"jobtitle" : "Developer",
 		"salary" : 2000.00,
 		},		{
 		"id": 10,
-		"firstname" : "Anton",
-		"lastname" : "Müller",
+		"firstname" : "Jessica",
+		"lastname" : "Karrer",
 		"emailadress" : "emailX@internet.com",
 		"jobtitle" : "Developer",
 		"salary" : 3000.00,
 		},
 
-	]
-};					
+	];
+					
 
-document.getElementById("output2").innerHTML = JSONob.employees[1].id;	// Testline
+// Test
+
+document.getElementById("output2").innerHTML = jsonOb[1];
+
+for (var i=0; i < jsonOb.length; i++){
+var objekt = jsonOb[i];
+var table = document.getElementById("ausgabe");
+var row = table.insertRow(i);
+var zelle1 = row.insertCell(0);
+var zelle2 = row.insertCell(1);
+var zelle3 = row.insertCell(2);
+var zelle4 = row.insertCell(3);
+var zelle5 = row.insertCell(4);
+var zelle6 = row.insertCell(5);
+
+zelle1.innerHTML = objekt.id;
+zelle2.innerHTML = objekt.firstname;
+zelle3.innerHTML = objekt.lastname;
+zelle4.innerHTML = objekt.emailadress;
+zelle5.innerHTML = objekt.jobtitle;
+zelle6.innerHTML = objekt.salary;
+
+}
+
+
