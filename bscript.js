899e124a2dabe401f80990853dde6cf4ee1c3cef
roman.cf <roman.springer@outlook.com>
2019-10-09 10:34:17 +0200

    basic

---
+++ bscript.js
@@ -0,0 +1,25 @@
+var sandwiches = { "sandwich": "hamburger", "calories": "260" };
+var fries = { "fries_size": "Large French Fries", "calories": "570" };
+
+
+
+
+document.getElementById("output1").innerHTML = "My favorite burger is a " 
+											+ sandwiches.sandwich
+											+ " which has approximately "
+											+ sandwiches.calories
+											+ " calories, along with it I enjoy eating "
+											+ fries.fries_size 
+											+ " which have about "
+											+ fries.calories
+											+ " calories."
+
+//---------------------------------------------------------------------------------------------
+
+var JSONob = {
+	"employees": [{
+
+
+
+	}]
+}											
